5b19cef004a8c57e22d56fa3112ec9ec5dec0683
QambarAli1 <qamberali4904@gmail.com>
2021-08-25 17:05:03 +0500

    updated

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const signUp = () => {
         localStorage.setItem("users", JSON.stringify(users));
         // redirect to login page
         location.href = "login.html";
-    } 
+    }
     else {
         message.innerHTML = user.email + " use in another account";
     }
@@ -51,32 +51,36 @@ const login = () => {
     var email = document.getElementById("email");
     var password = document.getElementById("password");
     var message = document.getElementById("message");
+    if (email.value != '' && password.value != '') {
+        var user = {
+            email: email.value,
+            password: password.value,
+        }
 
-    var user = {
-        email: email.value,
-        password: password.value,
+        var users = JSON.parse(localStorage.getItem("users")) || [];
+        // get indx
+        var currentUser = users.find(function (val) {
+            return val.email.toLowerCase() === user.email.toLowerCase() && val.password === user.password;
+        });
+
+        if (currentUser) {
+            localStorage.setItem("user", JSON.stringify(currentUser));
+            // user login
+            location.href = "dashboard.html";
+        } else {
+            message.innerHTML = "Invalid credentials";
+            message.style.color = 'red';
+            email.value = "";
+            password.value = "";
+        }
+        // clear state
+        setTimeout(() => {
+            message.innerHTML = "";
+        }, 2000);
     }
-
-    var users = JSON.parse(localStorage.getItem("users")) || [];
-    // get indx
-    var currentUser = users.find(function (val) {
-        return val.email.toLowerCase() === user.email.toLowerCase() && val.password === user.password;
-    });
-
-    if (currentUser) {
-        localStorage.setItem("user", JSON.stringify(currentUser));
-        // user login
-        location.href = "dashboard.html";
-    } else {
-        message.innerHTML = "Invalid credentials";
-        message.style.color = 'red';
-        email.value = "";
-        password.value = "";
+    if (email.value == '' || password.value == '') {
+        message.innerHTML = 'Fill All credentials'
     }
-    // clear state
-    setTimeout(() => {
-        message.innerHTML = "";
-    }, 2000);
 }
 
 
@@ -89,47 +93,49 @@ const logout = () => {
 
 
 
-var getCurrentUser = ()=>{
+var getCurrentUser = () => {
     var currentUserName = document.getElementById('currentUserName');
     var currentUserEmail = document.getElementById('currentUserEmail');
     var currentUserAddress = document.getElementById('currentUserAddress');
     var currentUserNumber = document.getElementById('currentUserNumber');
     var user = JSON.parse(localStorage.getItem("user"));
-    currentUserName.innerHTML = user.name;
-    currentUserEmail.innerHTML = user.email;
-    currentUserAddress.innerHTML = user.address;
-    currentUserNumber.innerHTML = user.number;
+    console.log(user);
+    if (user == null) {
+        location.href = 'login.html';
+    }
 }
 
-const post = ()=>{
+const post = () => {
     var title = document.getElementById('title');
     var description = document.getElementById('description');
 
-    var post = document.getElementById('postItem');
-    var divMain = document.createElement('div');
-    var h5 = document.createElement('h5');
-    var divInner = document.createElement('div');
-    var image = document.createElement('img');
-    var para = document.createElement('p')
+    if (title.value != '' && description.value != '') {
+        var post = document.getElementById('postItem');
+        var divMain = document.createElement('div');
+        var h5 = document.createElement('h5');
+        var divInner = document.createElement('div');
+        var image = document.createElement('img');
+        var para = document.createElement('p')
 
+        divMain.setAttribute("class", "card")
+        divMain.setAttribute("style", "width: 18rem;")
+        image.setAttribute("class", "card-img-top")
+        image.setAttribute("src", "images/user.jpg")
+        divInner.setAttribute("class", "card-body")
+        h5.setAttribute("class", "card-title")
+        para.setAttribute("class", "card-text")
 
-    divMain.setAttribute("class", "card")
-    divMain.setAttribute("style", "width: 18rem;")
-    image.setAttribute("class", "card-img-top")
-    image.setAttribute("src", "images/user.jpg")
-    divInner.setAttribute("class", "card-body")
-    h5.setAttribute("class", "card-title")
-    para.setAttribute("class", "card-text")
+        var h5Text = document.createTextNode(title.value);
+        h5.appendChild(h5Text)
+        var paraText = document.createTextNode(description.value);
+        para.appendChild(paraText)
+        divInner.appendChild(h5)
+        divInner.appendChild(para)
+        divMain.appendChild(image)
+        divMain.appendChild(divInner);
+        post.appendChild(divMain);
+        title.value = "";
+        description.value = "";
+    }
 
-    var h5Text = document.createTextNode(title.value);
-    h5.appendChild(h5Text)
-    var paraText = document.createTextNode(description.value);
-    para.appendChild(paraText)
-    divInner.appendChild(h5)
-    divInner.appendChild(para)
-    divMain.appendChild(image)
-    divMain.appendChild(divInner);
-    post.appendChild(divMain);
-    title.value ="";
-    description.value = "";
 }
